93d6b07d035c6dd697e0d98f17a7c3415bd9b310
Riyaa-k <anshitakoshta28@gmail.com>
2023-11-26 13:41:00 +0530

    Added button functionality

--- QuizApp/src/components/Quiz.jsx
+++ QuizApp/src/components/Quiz.jsx
@@ -1,8 +1,36 @@
 import React, { Component } from 'react'
 import "../styles/quiz.css"
+import data from '../Resources/Resources'
 
 export default class Quiz extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      currentQuestionIndex:0,
+    }
+  }
+
+  handlePrevious=()=>{
+    this.setState((prevState)=>({
+      currentQuestionIndex:prevState.currentQuestionIndex-1,
+    }))
+  }
+
+  handleNext=()=>{
+    this.setState((prevState)=>({
+      currentQuestionIndex:prevState.currentQuestionIndex+1,
+    }))
+  }
+
+  handleQuit=()=>{
+    confirm("Are you sure you want to quit ?");
+    window.location = "/";
+  }
+
   render() {
+    const {currentQuestionIndex}=this.state;
+    const currentQuestion=data[currentQuestionIndex];
+
     return (
       <div className='parentdiv'>
         <div className="game-container">
@@ -10,27 +38,27 @@ export default class Quiz extends Component {
             <div className="question-container">
                 <h3>Question</h3>
                 <div className="question-number">
-                    1 of 15
+                    {currentQuestionIndex + 1} of {data.length}
                 </div>
-                <p>Which is the only mammal that can jump?</p>
+                <p>{currentQuestion.question}</p>
                 
             </div>
 
             <div className="option-container">
                 <div className="option-parent">
-                    <div className="options">Dog</div>
-                    <div className="options">Elephant</div>
+                    <div className="options">{currentQuestion.optionA}</div>
+                    <div className="options">{currentQuestion.optionB}</div>
                 </div>
                 <div className="option-parent">
-                    <div className="options">Goat</div>
-                    <div className="options">Lion</div>
+                    <div className="options">{currentQuestion.optionC}</div>
+                    <div className="options">{currentQuestion.optionD}</div>
                 </div>
             </div>
 
             <div className="button-container">
-                <button className='Previous'>Previous</button>
-                <button className='Next'>Next</button>
-                <button className='Quit'>Quit</button>
+                <button className='Previous' onClick={this.handlePrevious} disabled={currentQuestionIndex ===0}>Previous</button>
+                <button className='Next' onClick={this.handleNext } disabled={currentQuestionIndex === data.length-1}>Next</button>
+                <button className='Quit' onClick={this.handleQuit}>Quit</button>
             </div>
         </div>
         
